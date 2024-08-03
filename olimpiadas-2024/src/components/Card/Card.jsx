ffcc147848a8c3424839c94fe5dc85963d9ef9cd
Lucineia Silva <90657609+LucineiaSilvah@users.noreply.github.com>
2024-08-03 00:58:46 -0300

    ajustando caminho das imagens Card.jsx

--- olimpiadas-2024/src/components/Card/Card.jsx
+++ olimpiadas-2024/src/components/Card/Card.jsx
@@ -1,21 +1,22 @@
 /* eslint-disable react/prop-types */
 import styles from './Card.module.css'
-
-
-const Card = ({dados}) => {
+import  mOuro from '/src/assets/img/medalha-de-ouro.png'
+import  mPrata from '/src/assets/img/medalha-de-prata.png'
+import  mBronze from '/src/assets/img/medalha-de-bronze.png'
+  const Card = ({dados}) => {
 
 
   return (
     <div className={styles.Card}>
       <img width={'100%'} src={dados.flag_url} alt="" />
      <span>
-     <img width={'30px'} src="/src/assets/img/medalha-de-ouro.png" alt="medalha-de-ouro"/>{dados.gold_medals}
+     <img width={'30px'} src={mOuro} alt="medalha-de-ouro"/>{dados.gold_medals}
      </span>
      <span>
-     <img width={'30px'} src="/src/assets/img/medalha-de-prata.png" alt="medalha-de-prata"/>{dados.silver_medals}
+     <img width={'30px'} src={mPrata} alt="medalha-de-prata"/>{dados.silver_medals}
      </span>
      <span>
-     <img width={'30px'} src="/src/assets/img/medalha-de-bronze.png" alt="medalha-de-bronze"/>{dados.bronze_medals}
+     <img width={'30px'} src={mBronze} alt="medalha-de-bronze"/>{dados.bronze_medals}
      </span>
     <span>TOTAL: {dados.total_medals}</span>
       <p>Nome: {dados.name}</p>
