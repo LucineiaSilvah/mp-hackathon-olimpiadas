7ebc066108ee117d39344b905d533064ab63ad18
luh <lucineia.rsilvah@gmail.com>
2024-08-06 22:14:49 -0300

    add imagem rodape

--- olimpiadas-2024/src/components/Rodape/Rodape.jsx
+++ olimpiadas-2024/src/components/Rodape/Rodape.jsx
@@ -1,10 +1,10 @@
 import styles from './Rodape.module.css';
-
+import logo from '../../assets/img/2024_Summer_Olympics_logo.svg.png';
 const Rodape = () => {
   return (
     <footer className={styles.Rodape}>
       <div>
-        <img className={styles.Logo} src="/src/assets/img/2024_Summer_Olympics_logo.svg.png" alt="logo olimpiadas" />
+        <img className={styles.Logo} src={logo} alt="logo olimpiadas" />
       </div>
       <div className={styles.Detalhes}>
 
